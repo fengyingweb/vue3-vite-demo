63b3808ee7b198bd93151274e59d6a6ef7462193
fengyingweb <31813362+fengyingweb@users.noreply.github.com>
2022-01-27 16:06:05 +0800

    Create vite.config.js

---
+++ vite.config.js
@@ -0,0 +1,55 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import styleImport, { VantResolve } from 'vite-plugin-style-import'
+const path = require('path')
+const glob = require('glob')
+
+function getEntrys() {
+  const entrys = {}
+  const pages = glob.sync(`${__dirname}/*.html`)
+  pages.forEach(pagePath=> {
+    let key = path.basename(pagePath, path.extname(pagePath))
+    let file = path.basename(pagePath)
+    entrys[key] = path.resolve(__dirname, file)
+  })
+  console.log(entrys)
+  return entrys
+}
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+    styleImport({
+      resolves: [VantResolve()],
+    })
+  ],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src') // 设置 `@` 指向 `src` 目录
+    }
+  },
+  base: './', // 设置打包路径
+  server: {
+    port: 3000,
+    host: '0.0.0.0',
+    open: false,
+    cors: true // 允许跨域
+  },
+  build: {
+    rollupOptions: {
+      input: getEntrys(),
+      output: {
+          chunkFileNames: 'assets/js/[name]-[hash].js',
+          entryFileNames: 'assets/js/[name]-[hash].js',
+          assetFileNames: 'assets/[ext]/[name]-[hash].[ext]'
+      },
+    },
+    terserOptions: {
+      compress: {
+        drop_console: true,
+        drop_debugger: true
+      }
+    }
+  }
+})
