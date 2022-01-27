741176edc190c7ed0e715d13a33e6d27838b1db3
fengyingweb <31813362+fengyingweb@users.noreply.github.com>
2022-01-27 16:30:55 +0800

    Create App.vue

---
+++ src/demo/views/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+</script>
+
+<template>
+  <img alt="Vue logo" src="@/assets/img/logo.png" />
+  <h1>Demo页面</h1>
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
