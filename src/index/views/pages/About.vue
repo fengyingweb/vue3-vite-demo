408c2ca9109915ab135856f9a5d4ac4d3f97ef7a
fengyingweb <31813362+fengyingweb@users.noreply.github.com>
2022-01-27 16:29:48 +0800

    Create About.vue

---
+++ src/index/views/pages/About.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <img alt="Vue logo" src="@/assets/img/logo.png" />
+    <h1>{{title}}</h1>
+  </div>
+</template>
+
+<script>
+import { defineComponent, ref } from 'vue'
+
+export default defineComponent({
+  setup() {
+    const title = ref('About 页面');
+    return {
+      title
+    }
+  }
+})
+</script>
+
+<style lang="less" scoped>
+
+</style>
