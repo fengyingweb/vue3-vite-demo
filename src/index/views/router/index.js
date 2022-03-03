63e28f5c1abc04c28fb61e3bce2bc5b9b1712644
fengyingweb <31813362+fengyingweb@users.noreply.github.com>
2022-03-03 13:58:41 +0800

    Update index.js

--- src/index/views/router/index.js
+++ src/index/views/router/index.js
@@ -1,7 +1,10 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
 
-const Home = ()=> import('../pages/Home.vue')
-const About = ()=> import('../pages/About.vue')
+const Home = ()=> import('../pages/Home')
+const MaterialDelivery = ()=> import('../pages/materialDelivery')
+const ReceiveWarehousing = ()=> import('../pages/receiveWarehousing')
+const CallEmptyCar = ()=> import('../pages/callEmptyCar')
+const PointToPointCall = ()=> import('../pages/pointToPointCall')
 
 const routes = [
   {
@@ -11,10 +14,28 @@ const routes = [
     meta: {title: '首页', keepAlive: true}
   },
   {
-    path: '/about',
-    name: 'About',
-    component: About,
-    meta: {title: '关于', keepAlive: false}
+    path: '/materialDelivery',
+    name: 'MaterialDelivery',
+    component: MaterialDelivery,
+    meta: {title: '呼叫1', keepAlive: false}
+  },
+  {
+    path: '/receiveWarehousing',
+    name: 'ReceiveWarehousing',
+    component: ReceiveWarehousing,
+    meta: {title: '呼叫2', keepAlive: false}
+  },
+  {
+    path: '/callEmptyCar',
+    name: 'CallEmptyCar',
+    component: CallEmptyCar,
+    meta: {title: '呼叫3', keepAlive: false}
+  },
+  {
+    path: '/pointToPointCall',
+    name: 'PointToPointCall',
+    component: PointToPointCall,
+    meta: {title: '呼叫4', keepAlive: false}
   }
 ];
 
